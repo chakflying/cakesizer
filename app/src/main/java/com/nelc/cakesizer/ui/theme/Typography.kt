beb27654e8b95b8f609545ebdf5a0eca082d0d5a
Nelson Chan <chakflying@hotmail.com>
2023-06-17 00:12:54 +0800

    Chore: Add italics font

--- app/src/main/java/com/nelc/cakesizer/ui/theme/Typography.kt
+++ app/src/main/java/com/nelc/cakesizer/ui/theme/Typography.kt
@@ -3,10 +3,13 @@ package com.nelc.cakesizer.ui.theme
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import com.nelc.cakesizer.R
 
 val Rubik = FontFamily(
+    Font(R.font.rubik_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.rubik_lightitalic, FontWeight.Light, FontStyle.Italic),
     Font(R.font.rubik_light, FontWeight.Light),
     Font(R.font.rubik_regular, FontWeight.Normal),
     Font(R.font.rubik_bold, FontWeight.Bold),
